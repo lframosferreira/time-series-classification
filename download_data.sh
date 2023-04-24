b28fb2b2509b571f35c59a96d23d4bbdd77e55ff
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-04-23 21:53:53 -0300

    [NEW] facedetection dataset added

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir data
+if [ ! -d data/ ]; then
+    mkdir data/
+fi
 
 # download LSST
 if [ ! -d "data/LSST/" ]; then
@@ -16,5 +18,21 @@ if [ ! -d "data/Tiselac" ]; then
     mkdir data/Tiselac/
     wget -P data/Tiselac http://timeseriesclassification.com/Downloads/Tiselac.zip
     unzip data/Tiselac/Tiselac.zip -d data/Tiselac/
-    rm data/Tiselac/*.zip data/Tiselac/*.png
+    rm data/Tiselac/*.zip data/Tiselac/*.jpg
+fi
+
+# download SpokenArabicDigits
+if [ ! -d "data/SpokenArabicDigits" ]; then
+    mkdir data/SpokenArabicDigits/
+    wget -P data/SpokenArabicDigits http://timeseriesclassification.com/Downloads/SpokenArabicDigits.zip
+    unzip data/SpokenArabicDigits/SpokenArabicDigits.zip -d data/SpokenArabicDigits/
+    rm data/SpokenArabicDigits/*.zip data/SpokenArabicDigits/*.jpg
+fi
+
+# download FaceDetection
+if [ ! -d "data/FaceDetection" ]; then
+    mkdir data/FaceDetection/
+    wget -P data/FaceDetection http://timeseriesclassification.com/Downloads/FaceDetection.zip
+    unzip data/FaceDetection/FaceDetection.zip -d data/FaceDetection/
+    rm data/FaceDetection/*.zip data/FaceDetection/*.jpg
 fi
