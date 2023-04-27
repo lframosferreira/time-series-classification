bf853acce9b975b6279d3fc583ad70d484888420
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-04-27 11:28:39 -0300

    awr

---
+++ src/ArticularyWordRecognition/AWR_model.py
@@ -0,0 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath("src"))
+
+from pipeline import *
+
+model_pipeline(prefix="results/rocket/NATOPS/all", model_name="Rocket_XGBoost", train_data_path="/scratch/luisfeliperamos/time-series-classification/data/NATOPS/NATOPS_TRAIN.arff", test_data_path="/scratch/luisfeliperamos/time-series-classification/data/NATOPS/NATOPS_TEST.arff", dimensions_to_use = None)
