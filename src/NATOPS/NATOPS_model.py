3b59789da3676809e6277eaa32232b7bba0d8e45
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2023-04-27 11:05:34 -0300

    diversity NATOPS

--- src/NATOPS/NATOPS_model.py
+++ src/NATOPS/NATOPS_model.py
@@ -4,4 +4,4 @@ sys.path.insert(0, os.path.abspath("src"))
 
 from pipeline import *
 
-model_pipeline(prefix="results/rocket/NATOPS/8_10_21_22", model_name="Rocket_XGBoost", train_data_path="/scratch/luisfeliperamos/time-series-classification/data/NATOPS/NATOPS_TRAIN.arff", test_data_path="/scratch/luisfeliperamos/time-series-classification/data/NATOPS/NATOPS_TEST.arff", dimensions_to_use = np.array([8, 10, 21, 22]))
+model_pipeline(prefix="results/rocket/NATOPS/8_10_11_12_16_17_21_22", model_name="Rocket_XGBoost", train_data_path="/scratch/luisfeliperamos/time-series-classification/data/NATOPS/NATOPS_TRAIN.arff", test_data_path="/scratch/luisfeliperamos/time-series-classification/data/NATOPS/NATOPS_TEST.arff", dimensions_to_use = np.array([8, 10, 21, 22]))
